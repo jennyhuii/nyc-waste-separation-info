5a1eaad1c27f88c7609e2ce32b6efaed04cb347e
Jenny <j.hui.0130@gmail.com>
2022-11-13 22:52:01 -0500

    lots of things

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
+import react from 'react';
+import styled from 'styled-components';
+import Logo from './NYC-waste-separation-info-logo.png';
+import Blue from './blue.png'
+import Green from './green.png'
 import './App.css';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const StyledDiv = styled.div`
+  padding: 1rem;
+  justify-content: center;
+`
+
+const StyledImage = styled.img`
+  width: 50%;  
+  align-self: center;
+`
+
+const StyledSign = styled.img`
+  width: 100%;  
+  align-self: center;
+`
+
+const StyledP = styled.p`
+  color: ${prop => prop.color};
+  font-size: ${prop => prop.fontSize};
+  font-weight: 600;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <StyledContainer>
+      <StyledImage src={Logo} alt='logo' />
+      <StyledDiv>
+        <StyledP color='#0015D7' fontSize='1rem'>
+          Metal, glass, plastic and cartons go in the blue bin (EMPTY AND RINSE if contains food/liquid!)
+        </StyledP>
+        <StyledSign src={Blue} alt='blue' />
+        <StyledP color='#47B81F' fontSize='1rem'>
+          Mixed paper and carboard go in the green bin (place in trash bin if your pizza box is too oily)
+        </StyledP>
+        <StyledSign src={Green} alt='green' />
+        <StyledP color='#434343' fontSize='1rem'>
+          Everything else goes in the garbage bin because NEW YORK CITY DOESN'T COMPOST, unfortunately.
+          Good news is we're working on it!
+        </StyledP>
+      </StyledDiv>
+      <StyledDiv>
         <p>
-          ARE YOU PUTTING YOUR TRASH IN THE RIGHT BIN?
+          More information coming soon...
         </p>
-
-      </header>
-    </div>
+      </StyledDiv>
+    </StyledContainer>
   );
 }
 
