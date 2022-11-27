24cd0b90d038c673e19a750e4d09ca41ba5db261
Jenny <j.hui.0130@gmail.com>
2022-11-27 11:32:46 -0500

    new words

--- src/pages/idm-progress.jsx
+++ src/pages/idm-progress.jsx
@@ -14,13 +14,13 @@ import {
 const data = [
     {
         name: '11/21',
-        IDM: 15,
-        ITP: 7,
+        IDM: 0,
+        ITP: 5,
     },
     {
         name: '11/22',
-        IDM: 6,
-        ITP: 9,
+        IDM: 0,
+        ITP: 5,
     },
     {
         name: '11/28',
@@ -53,12 +53,13 @@ const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  text-align: center;
+  padding: 0.5rem;
 `
 
 const StyledDiv = styled.div`
 padding: 1rem;
 justify-content: center;
+text-align: center;
 `
 
 const StyledImage = styled.img`
@@ -78,18 +79,28 @@ const IDMProgress = () => {
             <StyledImage src={Logo} alt='logo' />
             <StyledDiv>
                 <StyledP color='#434343' fontSize='1.5rem'>
-                    370 Jay St Recycling
-                </StyledP>
-                <StyledP color='#434343' fontSize='1rem'>
-                    Total Points:
-                </StyledP>
-                <StyledP color='#57068c' fontSize='1rem'>
-                    3rd Floor - IDM: 21
-                </StyledP>
-                <StyledP color='#8900e1' fontSize='1rem'>
-                    4th Floor - ITP: 16
+                    IDM vs ITP Waste Separation Competition!
                 </StyledP>
             </StyledDiv>
+            <div>
+                In an effort to improve waste separation habits at 370 Jay Street,
+                NYU has implemented a leadership board to keep score of which department is performing better:
+                ITP vs IDM. Each week, Facilities will inspect the trash bins on the 3rd and 4th floors and measure:
+                <p>
+
+                    (1) the percent accuracy of correct trash items per bin and
+                </p>
+                <p>
+
+                    (2) the cumulative weight of recyclables (so don't forget to empty the bottles!) by EOD.
+                </p>
+                At each bag change,
+                Facilities will award <b>5 points</b> to the department that scored the highest percent accuracy while
+                maintaining the smallest weight of accumulated recyclables. At the end of the day, points are tallied up for
+                each department and displayed on our website. The winner will be announced on the last day of classes for Fall 2022.
+                Best of luck, IDM!
+            </div>
+            <br/>
             <BarChart
                 width={375}
                 height={300}
@@ -103,6 +114,17 @@ const IDMProgress = () => {
                 <Bar dataKey="IDM" fill="#57068c" />
                 <Bar dataKey="ITP" fill="#8900e1" />
             </BarChart>
+            <StyledDiv>
+                <StyledP color='#434343' fontSize='1rem'>
+                    Total Points:
+                </StyledP>
+                <StyledP color='#57068c' fontSize='1rem'>
+                    3rd Floor - IDM: 0
+                </StyledP>
+                <StyledP color='#8900e1' fontSize='1rem'>
+                    4th Floor - ITP: 10
+                </StyledP>
+            </StyledDiv>
         </StyledContainer>
     )
 }
