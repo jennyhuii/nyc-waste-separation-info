6dc4445e692f0a92282ad9245b07c3f10ec71847
Jenny <j.hui.0130@gmail.com>
2022-11-26 20:52:21 -0500

    added bar charts

--- src/App.js
+++ src/App.js
@@ -1,63 +1,24 @@
 import styled from 'styled-components';
-import Logo from './NYC-waste-separation-info-logo.png';
-import Blue from './blue.png'
-import Green from './green.png'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Resource from './pages/resource';
+import IDMProgress from './pages/idm-progress';
+import ITPProgress from './pages/itp-progress';
 import './App.css';
-
-const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-`
-
-const StyledDiv = styled.div`
-  padding: 1rem;
-  justify-content: center;
-`
-
-const StyledImage = styled.img`
-  width: 50%;  
-  align-self: center;
-`
-
-const StyledSign = styled.img`
-  width: 100%;  
-  align-self: center;
-`
-
-const StyledP = styled.p`
-  color: ${prop => prop.color};
-  font-size: ${prop => prop.fontSize};
-  font-weight: 600;
-`
 
 function App() {
   return (
-    <StyledContainer>
-      <StyledImage src={Logo} alt='logo' />
-      <StyledDiv>
-        <p>
-          Hello! We're working on a more comprehensive and interactive guide to answer all your waste separation questions! 
-          For now, please refer to guidance below. 
-          Source: nyc.gov/zerowaste
-        </p>
-      </StyledDiv>
-      <StyledDiv>
-        <StyledP color='#0015D7' fontSize='1rem'>
-          Metal, glass, plastic and cartons go in the blue bin (EMPTY AND RINSE if contains food/liquid!)
-        </StyledP>
-        <StyledSign src={Blue} alt='blue' />
-        <StyledP color='#47B81F' fontSize='1rem'>
-          Mixed paper and carboard go in the green bin (place in trash bin if your pizza box is too oily)
-        </StyledP>
-        <StyledSign src={Green} alt='green' />
-        <StyledP color='#434343' fontSize='1rem'>
-          Everything else goes in the garbage bin because NEW YORK CITY DOESN'T COMPOST, unfortunately.
-          Good news is we're working on it!
-        </StyledP>
-      </StyledDiv>
-    </StyledContainer>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Resource />} />
+        <Route path="/idm-recycling" element={<IDMProgress />} />
+        <Route path="/itp-recycling" element={<ITPProgress />} />
+      </Routes>
+    </Router>
+
   );
 }
 
---
+++ src/pages/itp-progress.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from './NYC-waste-separation-info-logo.png';
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+} from "recharts";
+
+const data = [
+    {
+        name: '11/21',
+        IDM: 15,
+        ITP: 7,
+    },
+    {
+        name: '11/22',
+        IDM: 6,
+        ITP: 9,
+    },
+    {
+        name: '11/28',
+        IDM: 0,
+        ITP: 0,
+    },
+    {
+        name: '11/29',
+        IDM: 0,
+        ITP: 0,
+    },
+    {
+        name: '11/30',
+        IDM: 0,
+        ITP: 0,
+    },
+    {
+        name: '12/1',
+        IDM: 0,
+        ITP: 0,
+    },
+    {
+        name: '12/2',
+        IDM: 0,
+        ITP: 0,
+    },
+];
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`
+
+const StyledDiv = styled.div`
+padding: 1rem;
+justify-content: center;
+`
+
+const StyledImage = styled.img`
+width: 50%;  
+align-self: center;
+`
+
+const StyledP = styled.p`
+color: ${prop => prop.color};
+font-size: ${prop => prop.fontSize};
+font-weight: 600;
+`
+
+const ITPProgress = () => {
+    return (
+        <StyledContainer>
+            <StyledImage src={Logo} alt='logo' />
+            <StyledDiv>
+                <StyledP color='#434343' fontSize='1.5rem'>
+                    370 Jay St Recycling
+                </StyledP>
+                <StyledP color='#434343' fontSize='1rem'>
+                    Total Points:
+                </StyledP>
+                <StyledP color='#57068c' fontSize='1rem'>
+                    3rd Floor - IDM: 21
+                </StyledP>
+                <StyledP color='#8900e1' fontSize='1rem'>
+                    4th Floor - ITP: 16
+                </StyledP>
+            </StyledDiv>
+            <BarChart
+                width={375}
+                height={300}
+                data={data}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="IDM" fill="#57068c" />
+                <Bar dataKey="ITP" fill="#8900e1" />
+            </BarChart>
+        </StyledContainer>
+    )
+}
+
+export default ITPProgress;
